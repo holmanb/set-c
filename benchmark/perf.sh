a7e33cd006958277e0050fb715fad31b8d3abcd7
Brett Holman <bpholman5@gmail.com>
2021-05-15 22:13:50 -0600

    appease shellcheck

--- benchmark/perf.sh
+++ benchmark/perf.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -x
 
-[ -z $1 ] && echo "need to pass test executiable" && exit 1
+[ -z "$1" ] && echo "need to pass test executiable" && exit 1
 
-FULL=$(realpath $0)
-ABS=$(dirname $FULL)
-EXE=`echo $1 | awk -F\/ '{print $NF}'`
+FULL=$(realpath "$0")
+ABS=$(dirname "$FULL")
+EXE=$(echo "$1" | awk -F/ '{print $NF}')
 
 DATADIR="$ABS/data"
 TMPDIR="$DATADIR/tmp"
@@ -14,10 +14,10 @@ SVGDIR="$DATADIR/svg"
 EXEDATA="$TMPDIR/$EXE.data"
 PERFDATA="$TMPDIR/$EXE.perf"
 
-mkdir $DATADIR -p
-mkdir $TMPDIR -p
-mkdir $SVGDIR -p
+mkdir "$DATADIR" -p
+mkdir "$TMPDIR" -p
+mkdir "$SVGDIR" -p
 
 perf record -F 99 -g -o "$EXEDATA" "$1"
 perf script -i "$EXEDATA" > "$PERFDATA"
-$ABS/FlameGraph/stackcollapse-perf.pl "$PERFDATA" | $ABS/FlameGraph/flamegraph.pl  > "$SVGDIR/$EXE.svg"
+"$ABS/FlameGraph/stackcollapse-perf.pl" "$PERFDATA" | "$ABS/FlameGraph/flamegraph.pl"  > "$SVGDIR/$EXE.svg"
